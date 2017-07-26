cf8d9c9b92ef20c05a6c04bb0d058b04d4945d6a
jellyfishOne <jrenteria.camino@gmail.com>
2017-07-26 16:29:09 -0700

    Created myFirstIO.js

--- myFirstIO.js
+++ myFirstIO.js
@@ -2,9 +2,12 @@
 Write a program that uses a single synchronous filesystem operation
 to read a file and print the number of newlines (\n) it contains
 to the console, similar to running car file | wc -1
+
+The full path of the file to read will be provided as the first command-line
+argument (i.e., process.argv[2]). You do not need to make your own test file.
 */
 
-//load fs module to perform fylesystem operations
+//load fs module to perform filesystem operations
 var fs = require('fs');
 
 //read file directory from command line
@@ -14,30 +17,8 @@ var fileDir = process.argv[2];
 var buf = fs.readFileSync(fileDir);
 
 //convert the bufer into a string
-var str = buf.toString();
-
-//split string, ends up with one more element than the number
-//of new lines.
-var arr = str.split("\n");
-
-//count number of new lines
-var newLine = 0;
-for(var i = 0; i < arr.length -1; i++){
-  newLine ++;
-}
+//split string, ends up with one more element than the number of new lines.
+var str = buf.toString().split("\n").length - 1;
 
 //print result
-console.log(newLine);
-
-//////////////////////Given Solution///////////////////////////////
-
-    var fs = require('fs')
-
-    var contents = fs.readFileSync(process.argv[2])
-    var lines = contents.toString().split('\n').length - 1
-    console.log(lines)
-
-    // note you can avoid the .toString() by passing 'utf8' as the
-    // second argument to readFileSync, then you'll get a String!
-    //
-    // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+console.log(str);
